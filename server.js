48fad9d90c5e7520c1991c26c1e51930bb858f75
Oscar Mejias <mejiasoscar1990@gmail.com>
2023-11-06 11:07:51 +1100

    make it better

--- server.js
+++ server.js
@@ -1,65 +1,54 @@
-require ('dotenv').config()
+require('dotenv').config()
 
-const express = require ('express')
+const express = require('express')
 const app = express()
 
-const jwt = require ('jsonwebtoken')
+const jwt = require('jsonwebtoken')
 
-app.use(express.json())
+app.use(express.urlencoded({ extended: true })) // Parse x-www-form-urlencoded data
 
-const posts = [{
-    
+const posts = [
+  {
     user_token: '12345',
     nombre: 'Jose Altuve',
     email: 'mejiasoscar1990@gmail.com',
-    
-},
-{
+  },
+  {
     user_token: '67890',
     nombre: 'Joseito',
     email: 'jose@example.com',
-    
-},
-
-{
+  },
+  {
     user_token: '1990',
     nombre: 'Oriana',
     email: 'oriana@example.com',
-    
-}
-
-
-
+  },
 ]
 
+app.post('/login', (req, res) => {
+  // Authenticate the user
 
+  const timestamp = Math.round(new Date().getTime() / 1000)
+  const jtinumber = (timestamp / 100) * 53
+  const jti = Math.round(jtinumber)
 
-app.post('/login', (req,res) => {
+  const userToken = req.body.user_token; // Get user_token from the request body
 
-    //Authenticate the user
+  const username = posts.find((user) => user.user_token === userToken);
 
-    const timestamp = Math.round((new Date()).getTime()/1000)
-    const jtinumber = (timestamp/100)*53;
-    const jti = Math.round(jtinumber);
+  if (!username) {
+    return res.status(401).json({ error: 'User not found' });
+  }
 
-    const username = posts.find(username => username.user_token === req.body.user_token)
+  const user = {
+    name: username.nombre,
+    email: username.email,
+    iat: timestamp,
+    jti: jti,
+  }
 
-    //if (!username) {
-    //    return res.status(401).json({ error: "User not found" });
-   // }
-
-    const user = {
-        
-        name:username.nombre,
-        email:username.email,
-        iat: timestamp,
-        jti: jti
-    }
-
-    const accessToken = jwt.sign (user, process.env.ACCESS_TOKEN_SECRET)
-    res.json({jwt: accessToken})
-
+  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+  res.json({ jwt: accessToken })
 })
-
 
 app.listen(process.env.PORT || 3000)
